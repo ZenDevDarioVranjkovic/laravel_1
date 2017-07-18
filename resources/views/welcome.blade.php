9343a39b10e4b9d1fda1f195f7704196f17edda1
ZenDevDarioVranjkovic <dario@zendev.se>
2017-07-18 14:10:50 +0200

    Control Structures

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>Some content</h1>
-    {{ 2 == 2 ? "hello" : "Does not equal" }}
+    <div class="row">
+        <col-md-12>
+            <h1>Control Structures</h1>
+                @if(true)
+                <p>This only displays if it is true</p>
+                @else
+                <p>This only displays if it is false</p>
+                @endif
+
+                @for($i = 0; $i < 5; $i++)
+                    <p>{{ $i + 1}}. Iteration</p>
+                @endfor
+        </col-md-12>
+    </div>
 @endsection
