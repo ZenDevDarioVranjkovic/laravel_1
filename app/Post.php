8b73e5786e70ae34cfb1968a2d4bf80e0ff72689
ZenDevDarioVranjkovic <dario@zendev.se>
2017-07-20 09:18:48 +0200

    Add methods

---
+++ app/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+class Post
+{
+    public function getPosts($session)
+    {
+        if(!$session->has('posts')){
+            $this->createDummyData($session);
+        }
+        return $session->get('posts');
+    }
+    private function createDummyData($session){
+        $post=[
+            [
+                'title' => 'Learning Laravel',
+                'content' => 'This blog post will get right on rrack with Laravel'
+            ],
+            [
+                'title' => 'Something else',
+                'content' => 'Some other content'
+            ]
+        ];
+        $session->put('posts', $post);
+    }
+}
