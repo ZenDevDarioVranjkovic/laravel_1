428bcdd4dcfbdaf2948332eb271f6fbe305bc160
ZenDevDarioVranjkovic <dario@zendev.se>
2017-07-21 13:02:06 +0200

    Post like

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Like;
 use App\Post;
 use Illuminate\Http\Request;
 use App\Http\Requests;
@@ -27,6 +28,14 @@ class PostController extends Controller
         return view('blog.post', ['post' => $post]);
     }
 
+    public function getLikePost($id)
+    {
+        $post = Post::find($id);
+        $like = new Like();
+        $post->likes()->save($like);
+        return redirect()->back();
+    }
+
     public function getAdminCreate()
     {
         return view('admin.create');
@@ -41,6 +50,7 @@ class PostController extends Controller
     public function getAdminDelete($id)
     {
         $post = Post::find($id);
+        $post->likes()->delete();
         $post->delete();
         return redirect()->route('admin.index')->with('info', 'Post delete');
     }
