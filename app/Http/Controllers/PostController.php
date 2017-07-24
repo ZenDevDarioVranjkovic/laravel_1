f2588d72b76735a3e53e6c84a60acdcb89e63aec
ZenDevDarioVranjkovic <dario@zendev.se>
2017-07-24 10:37:27 +0200

    Connecting Users and Post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,9 +5,9 @@ namespace App\Http\Controllers;
 use App\Like;
 use App\Post;
 use App\Tag;
+use Auth;
 use Illuminate\Http\Request;
 use App\Http\Requests;
-
 
 class PostController extends Controller
 {
@@ -65,11 +65,15 @@ class PostController extends Controller
             'title' => 'required|min:5',
             'content' => 'required|min:10'
         ]);
+        $user = Auth::user();
+        if(!$user){
+            return redirect()->back();
+        }
         $post = new Post([
             'title' => $request->input('title'),
             'content' => $request->input('content')
         ]);
-        $post->save();
+        $user->posts()->save($post);
         $post->tags()->attach($request->input('tags') === null ? [] : $request->input('tags'));
 
         return redirect()->route('admin.index')->with('info', 'Post created, Title is: ' . $request->input('title'));
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -10,7 +10,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PostTableSeeder::class);
+//        $this->call(PostTableSeeder::class);
         $this->call(TagTableSeeder::class);
     }
 }
